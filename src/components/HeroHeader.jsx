4ef6d7e4ff3b5def43ec86c8fb83f96da1ca10c8
Christian Peters <c.peters@makermaker.de>
2024-04-12 11:16:35 +0200

    Herotext geändert

--- src/components/HeroHeader.jsx
+++ src/components/HeroHeader.jsx
@@ -2,7 +2,7 @@ export default function HeroHeader() {
     return (
         <header className="flex flex-col justify-center items-center min-h-screen">
             <h1 className="text-3xl">The BookStore</h1>
-            <h3>Was wir haben, haben wir</h3>
+            <h3>Wir haben Bücher</h3>
         </header>
     )
 }
